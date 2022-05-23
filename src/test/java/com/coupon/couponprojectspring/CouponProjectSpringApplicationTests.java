f8fe013f9d8c4cb0b5d83f39fd7d3a1a8a473303
OmerCohen993 <omerplace@gmail.com>
2022-05-23 14:41:41 +0300

    git init

---
+++ src/test/java/com/coupon/couponprojectspring/CouponProjectSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coupon.couponprojectspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponProjectSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
